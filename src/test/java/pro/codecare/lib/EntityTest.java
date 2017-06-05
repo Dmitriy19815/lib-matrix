1b9ac53b9c4f5520d87846e09e23c4b3e5699997
Dmytro Farafonov <fdv.nipi@gmail.com>
2017-06-05 10:34:34 +0300

    Update EntityTest.java

--- src/test/java/pro/codecare/lib/EntityTest.java
+++ src/test/java/pro/codecare/lib/EntityTest.java
@@ -1,15 +1,7 @@
-/*
- * Copyright 2017 Key Bridge. All rights reserved.
- * Use is subject to license terms.
+/**
+ * Unit cases tests to show how Matrix utility logic works
  *
- * Software Code is protected by Copyrights. Author hereby reserves all rights
- * in and to Copyrights and no license is granted under Copyrights in this
- * Software License Agreement.
- *
- * Key Bridge generally licenses Copyrights for commercialization pursuant to
- * the terms of either a Standard Software Source Code License Agreement or a
- * Standard Product License Agreement. A copy of either Agreement can be
- * obtained upon request from: info@keybridgewireless.com
+ * @author Dmytro Farafonov
  */
 package pro.codecare.lib;
 
@@ -19,11 +11,6 @@ import org.junit.Test;
 import java.io.FileWriter;
 import java.io.IOException;
 
-/**
- * Unit cases tests to show how Matrix utility logic works
- *
- * @author Dmytro Farafonov
- */
 public class EntityTest {
 
   public EntityTest() {
